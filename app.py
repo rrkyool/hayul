0716fc4e380646df7b69ad25e253572149962587
Park Hayul <phy6487043@naver.com>
2026-04-26 14:41:52 +0900

    Update app.py

--- app.py
+++ app.py
@@ -137,10 +137,12 @@ with top_right:
         c1, c2 = st.columns(2)
         with c1:
             m_type = st.selectbox("예측 모델", ["이동평균", "지수평활", "Holt-Winters", "ARIMA", "SARIMA"])
-            h_len = st.number_input("예측 길이(시평)", 5, 100, 12)
-        with c2:
-            u_type = st.selectbox("시간 단위", ["일", "주", "월", "년"], index=2)
             e_type = st.selectbox("평가 방식", ["Rolling", "Expanding"])
+            
+        with c2:
+            h_len = st.number_input("예측 길이(시평)", 5, 100, 12)
+            u_type = st.selectbox("시간 단위", ["일", "주", "월", "년"], index=2)
+            
         
         st.write("") # 간격
         btn_run = st.button("🚀 예측 실행", use_container_width=True, type="primary")
